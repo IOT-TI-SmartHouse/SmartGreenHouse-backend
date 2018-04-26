56b6c018d6134653b5b761643f6fa1cd95018e15
unknown <bartmachielsen@gmail.com>
2018-04-26 14:47:31 +0200

    * removed duplicate code

--- src/routes/sensordata.js
+++ src/routes/sensordata.js
@@ -5,40 +5,30 @@ const SensorNode = require("../models/sensorNode")
 const Sensordata = require("../models/sensorData")
 
 router.post("/register", verifyToken, (req, res) => {
-    SensorNode.findById(req.body.node).then(
-        node => {
-            SensorNode.canEdit(node.greenhouseDepartment, req.userId).then(
-                _ => {
-                    Sensordata.create({
-                        sensorType: req.body.sensorType,
-                        value: req.body.value,
-                        node: node._id
-                    }).then(
-                        data => res.status(200).send({id:data._id})
-                    )
-                },
-                error => res.status(500).send("[Sensordata::register] error registering Sensordata : " + error)
+    SensorNode.canEdit(req.body.node, req.userId).then(
+        _ => {
+            Sensordata.create({
+                sensorType: req.body.sensorType,
+                value: req.body.value,
+                node: node._id
+            }).then(
+                data => res.status(200).send({id:data._id})
             )
-        },
-        error => res.status(500).send("[Sensordata::register] error registering Sensordata : " + error)
+        }
     )
 });
 
 router.get("/getAll", verifyToken, (req, res) => {
-    SensorNode.findById(req.body.node).then(
-        node => {
-            SensorNode.canEdit(node.greenhouseDepartment, req.userId).then(
-                _ => {
-                    Sensordata.find({node: node.id}).then(
-                        data => res.status(200).send({data:data}),
-                        error => res.status(500).send("[Sensordata::getAll] error getting Sensordata : " + error)
-                    )
-                },
+    SensorNode.canEdit(req.body.node, req.userId).then(
+        _ => {
+            Sensordata.find({node: node.id}).then(
+                data => res.status(200).send({data:data}),
                 error => res.status(500).send("[Sensordata::getAll] error getting Sensordata : " + error)
             )
         },
         error => res.status(500).send("[Sensordata::getAll] error getting Sensordata : " + error)
     )
+
 });
 
 module.exports = router;
