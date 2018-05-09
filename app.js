ea3884d1cd1466cb903547d441b569d9b78cbc0a
Berend Vet <kantoor+berend@asamco.com>
2018-05-09 09:53:53 +0200

    Auto stash before merge of "develop" and "origin/develop"
    headers all all

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ repository.connect();
 // Allow cross origin requests
 app.all('/*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "*");
     next();
   });
 
