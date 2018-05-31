0bd952a4d23aa900620ef7128a6786f661913f00
bartmachielsen <bartmachielsen@gmail.com>
2018-05-31 10:53:07 +0200

    * Added getting admin state when logging into the server

--- src/routes/users.js
+++ src/routes/users.js
@@ -57,7 +57,7 @@ router.post("/login", function(req, res) {
       expiresIn: 86400 // expires in 24 hours
     });
 
-    res.status(200).send({ auth: true, token: token });
+    res.status(200).send({ auth: true, token: token, isAdmin:user.isAdmin});
   });
 });
 
