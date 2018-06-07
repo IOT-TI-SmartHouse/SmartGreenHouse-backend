b6d38d775fabf49bc2c787f62e1e564c9957f211
Bart Machielsen <bartmachielsen@MacBook-Pro-van-Bart.local>
2018-06-07 13:36:42 +0200

    *changed delete

--- src/routes/access.js
+++ src/routes/access.js
@@ -59,7 +59,7 @@ router.post("/delete", verifyToken, function(req, res) {
           }
         });
       } else if (req.body.user && req.body.greenhouse) {
-        GreenhouseAccess.deleteOne(
+        GreenhouseAccess.remove(
           { greenhouse: req.body.greenhouse, user: req.body.user },
           (error, _) => {
             if (error) {
